10214ad6efbee5b0acc34557e7b9331741405e49
nfongHang <fngai@kellettschool.com>
2024-10-07 04:24:29 +0000

    Oct 7, 2024, 12:24 PM

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import requests
+entering_url=True
+while entering_url:
+    website=str(input("Enter Website\n > "))
 
-website=str(input("Enter Website\n > "))
-
-if not "https://" in website:
-    website="https://"+website
-try:                                  # tries to fetch website
-    x = requests.get(website)         # when it responds with <Response [200]>, it means it connected successfully
-                                      # creates object of class "requests.models.Response"
-                                            # attributes of class:
-                                                # .content                   stores the html data of site
-                                                # .text                      stores the html data of site, in unicode
-                                                # .apparent_encoding         holds the encoding format of text (utf-8, unicode, ASCII)   
-    
-except:
-    print("Invalid URL")
+    if not "https://" in website:
+        website="https://"+website
+    try:                                  # tries to fetch website
+        x = requests.get(website)         # when it responds with <Response [200]>, it means it connected successfully
+                                        # creates object of class "requests.models.Response"
+                                                # attributes of class:
+                                                    # .content                   stores the html data of site
+                                                    # .text                      stores the html data of site, in unicode
+                                                    # .apparent_encoding         holds the encoding format of text (utf-8, unicode, ASCII)   
+        
+        if x.status_code == 200:        # Checks if status code is 200 (connection OK)
+            entering_url=False
+        else:
+            print('Failed to retrieve the webpage. Status code:', response.status_code)
+    except:
+        print("Invalid URL")
