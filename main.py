d264b2ba469eff7b0bbc50c521908de130a9b8bc
nfongHang <fngai@kellettschool.com>
2024-10-07 04:14:35 +0000

    Oct 7, 2024, 12:14 PM

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ try:                                  # tries to fetch website
     x = requests.get(website)         # when it responds with <Response [200]>, it means it connected successfully
                                       # creates object of class "requests.models.Response"
                                             # attributes of class:
-                                                # [object] .content      stores the html data of site
-                                                # 
-    print(x.apparent_encoding)
+                                                # .content                   stores the html data of site
+                                                # .apparent_encoding         holds the encoding format of text (utf-8, unicode, ASCII)   
+    print(x.url)
 except:
     print("Invalid URL")
