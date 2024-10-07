d93d6fd4dbf0a8d08422b92a0c3b8df362e8ee76
Nick-Akov <nickakov06@gmail.com>
2024-10-07 08:51:12 +0000

    Oct 7, 2024, 4:51 PM

--- keywordsearch.py
+++ keywordsearch.py
@@ -4,5 +4,9 @@ KeywordList = []
 print("enter keywords")
 while True:
     # We need to put an if statement here or smth so the loop doesnt go on foreber
-    KeywordList.append(str(input("> ")).lower())
+    if str(input("> ")).lower() == "\n":
+        KeywordList.append(str(input("> ")).lower())
+
+# How will the inputs happen
 re.search()
+# We can turn this into a function in th emain body
