bf989538fbb1e3bacc8624699a86a81a2a6f4c3c
nfongHang <fngai@kellettschool.com>
2024-10-07 04:06:13 +0000

    Oct 7, 2024, 12:06 PM

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
-
-x = requests.get("https://youtube.com/home")
-print(x)
+website=str(input("Enter Website\n > "))
+if not "https://" in website:
+    website="https://"+website
+try:
+    x = requests.get(website)         # when it responds with <Response [200]>, it means it connected successfully
+    print(x)
+except:
+    print("Invalid URL")
