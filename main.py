da25f37cfb86ad4029146bef512f4336c91479e9
nfongHang <fngai@kellettschool.com>
2024-10-07 04:07:10 +0000

    Oct 7, 2024, 12:07 PM

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+
 website=str(input("Enter Website\n > "))
+
 if not "https://" in website:
     website="https://"+website
-try:
+try:                                  # tries to fetch website
     x = requests.get(website)         # when it responds with <Response [200]>, it means it connected successfully
-    print(x)
+    print(x.content)
 except:
     print("Invalid URL")
