b8c3b4b864230ee0541da7b90bcb4adba7af0ba5
Nick-Akov <nickakov06@gmail.com>
2024-10-07 11:38:55 +0000

    Oct 7, 2024, 7:38 PM

--- keywordsearch.py
+++ keywordsearch.py
@@ -1,17 +1,16 @@
 import re
 #keyword search
 def keyWordSearch():
-    KeywordList = []
+    Keyword = ''
     print("enter keywords")
     while True:
         # We need to put an if statement here or smth so the loop doesnt go on foreber
         KeywordInput = str(input("> "))
         if KeywordInput != " ":
-            KeywordList.append(KeywordInput.lower())
+            #KeywordList.append(KeywordInput.lower())
         else: 
             break
     return keyWordSearch
 
 # How will the inputs happen
-re.search()
 # We can turn this into a function in th emain body
