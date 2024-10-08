fde31bcdbd2d444d6d4dfa0f08733c8722c55da2
Nick-Akov <112479952+Nick-Akov@users.noreply.github.com>
2024-10-08 09:18:00 +0800

    Create regExWordSearcher.py

---
+++ regExWordSearcher.py
@@ -0,0 +1,9 @@
+import re
+
+def regExWordSearch (placeHolderString, placeHolderList):
+    count = 0
+    for i in placeHolderList:
+        if re.search(i, placeHolderString):
+            count += 1
+    if count == len(placeHolderList):
+        return True 
