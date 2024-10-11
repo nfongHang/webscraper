fc5f4ce0650188f1539a1203196df8689a17c179
Nick-Akov <nickakov06@gmail.com>
2024-10-11 14:26:21 +0800

    check

--- keywordsearch.py
+++ keywordsearch.py
@@ -18,22 +18,22 @@ def keyWordSearch():
 # We can turn this into a function in th emain body
 
 
-# def regExWordSearch (placeHolderString, placeHolderList):
-#     for i in placeHolderList:
-#         print(i)
-#         if not re.search(i, placeHolderString):
-#             if input(str(i,"was not found in the website html. Proceed? Y/N")) == "N":
-#                 return False
-#     print("All %s words are within html." % len(placeHolderList))
-#     return True 
+def regExWordSearch (placeHolderString, placeHolderList):
+    for i in placeHolderList:
+        print(i)
+        if not re.search(i, placeHolderString):
+            if input(str(i,"was not found in the website html. Proceed? Y/N")) == "N":
+                return False
+    print("All %s words are within html." % len(placeHolderList))
+    return True 
 
 
 def searchKeyWords(KeyWordList):
     big_string = ''
     for i in KeyWordList:
         big_string += ' ' + i
-    entering = webdriver.find_elements_by_class_name("ant-input ant-input-lg")
-    entering.send_keys(big_string)
-    entering.send_keys(Keys.ENTER)
+    #entering = webdriver.find_elements_by_class_name("ant-input ant-input-lg") is there annother atrtribute we can use to fing the text box
+    #entering.send_keys(big_string)
+    #entering.send_keys(Keys.ENTER)
 
 
