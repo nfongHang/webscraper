8403fd59a768d1ea2e36ddef29cdc4321408b162
nfongHang <fngai@kellettschool.com>
2024-11-05 06:48:11 +0000

    Nov 5, 2024, 2:48 PM

--- keywordsearch.py
+++ keywordsearch.py
@@ -22,7 +22,7 @@ def lookup (placeHolderString, placeHolderList):
     lookupDict={}
     unmatchedCount=0
     for i in placeHolderList:
-        expression=re('[^\./<>",]*(\b%s\b)[^\./<>",]+\.?' % i)
+        expression=r'[^\./<>",]*(\b' + i + r'\b)[^\./<>",]+\.?'
         print(expression)
         matches=re.findall( re.compile((expression), re.I) , placeHolderString)
         print(matches)
